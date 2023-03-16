d39370b3f4c6b18dd697c1cf1a120eddc8707eb3
agnesgiovanni <81547269+agnesgiovanni@users.noreply.github.com>
2023-03-16 11:59:29 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import indexScreen from "./src/screens/indexScreen";
+import { Provider } from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
+
+const navigator = createStackNavigator(
+  {
+    Index: indexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
+    Edit : EditScreen
+  }, 
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: "Blogs"
+    }
+  }
+);
+
+const App =  createAppContainer(navigator);
+
+export default () => {
+  return (
+  <Provider>
+    <App/>
+  </Provider>
+  );
+}
---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: ' http://8e1c-111-94-72-169.ngrok.io'
+})
